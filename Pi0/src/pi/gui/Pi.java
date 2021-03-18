adab84b9a0f071310bd4b0dfa4cd88925bdd5bfc
wael <wael.amri1@esprit.tn>
2021-03-18 22:04:11 +0100

    Second commit

--- Pi0/src/pi/gui/Pi.java
+++ Pi0/src/pi/gui/Pi.java
@@ -22,7 +22,6 @@ public class Pi extends Application {
     public void start(Stage stage) throws Exception {
        
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-         
         Scene scene = new Scene(root);
         
         stage.setResizable(false);
